6e1f6094ffdd43ba028279e862edebba08d5e1b1
Yashodeep Deshpande <yashodeepdeshpande84639@gmail.com>
2026-02-21 10:54:28 +0530

    Working of displaying the list of todos

--- todolist/src/pages/Addtodo.jsx
+++ todolist/src/pages/Addtodo.jsx
@@ -1,6 +1,45 @@
 import { useState } from 'react'
 import './Addtodo.css'
 
+// const tasks = [
+//   {
+//     id: 1,
+//     title: "Quant Practice",
+//     description: "Maths, English, Reasoning",
+//     priority: "Low"
+//   },
+//   {
+//     id: 2,
+//     title: "DSA Practice",
+//     description: "Solve 3 array problems and 2 string problems",
+//     priority: "High"
+//   },
+//   {
+//     id: 3,
+//     title: "React Revision",
+//     description: "Revise useState, useEffect and build small project",
+//     priority: "Medium"
+//   },
+//   {
+//     id: 4,
+//     title: "System Design Basics",
+//     description: "Learn about load balancing and caching",
+//     priority: "Medium"
+//   },
+//   {
+//     id: 5,
+//     title: "Mock Interview",
+//     description: "Practice coding interview questions for 1 hour",
+//     priority: "High"
+//   },
+//   {
+//     id: 6,
+//     title: "English Speaking Practice",
+//     description: "30 minutes English conversation practice",
+//     priority: "Low"
+//   }
+// ];
+
 function AddTodo() {
 
   const [formData, setFormData] = useState({
@@ -9,6 +48,8 @@ function AddTodo() {
     duedate: "",
     priority: "low"
   })
+
+  const [Tasks,setTasks] = useState([])
 
   const handleChange = (e) => {
     const { name, value } = e.target
@@ -20,9 +61,15 @@ function AddTodo() {
   }
 
   const handleSubmit = (e) => {
-    e.preventDefault()
-    console.log(formData)
+  e.preventDefault()
+
+  const newTask = {
+    id: Date.now(),
+    ...formData
   }
+
+  setTasks(prev => [...prev, newTask])
+}
 
   return (
     <div className="container">
@@ -79,6 +126,31 @@ function AddTodo() {
         <button type="submit">Submit</button>
 
       </form>
+
+       <div className="listoftodos">
+
+        <div className="listoftodosheader">
+          <h2>List of Todos</h2>
+        </div>
+
+        <ul className='listofitems'>
+        
+
+      {Tasks.map(Task => (
+      <li key={Task.id}>
+      <h3>{Task.title}</h3>
+      <p>{Task.description}</p>
+      <span>Priority:{Task.priority}</span>
+    </li>
+  ))}
+        </ul>
+
+
+
+       </div>
+
+
+
     </div>
   )
 }
